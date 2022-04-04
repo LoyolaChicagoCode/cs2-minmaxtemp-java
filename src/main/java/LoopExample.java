8182ff5701c131a78e1a6c5d750fb221f4ab8288
Konstantin Läufer <laufer@cs.luc.edu>
2022-04-04 17:14:42 -0500

    fixed Java formatting
    Signed-off-by: Konstantin Läufer <laufer@cs.luc.edu>

--- src/main/java/LoopExample.java
+++ src/main/java/LoopExample.java
@@ -1,15 +1,14 @@
 public class LoopExample {
 
-    public static void main(final String[] args) {
+  public static void main(final String[] args) {
 
-        System.out.println("hello there!");
+    System.out.println("hello there!");
 
-        System.out.println("Number of args: " + args.length);
+    System.out.println("Number of args: " + args.length);
 
-        // counting loop: we know beforehand how many times it will repeat
-        for (int i = 0; i < args.length; i++) {
-            System.out.println("args[" + i + "] = " + args[i]);
-        }
+    // counting loop: we know beforehand how many times it will repeat
+    for (int i = 0; i < args.length; i++) {
+      System.out.println("args[" + i + "] = " + args[i]);
     }
-
+  }
 }
--- src/main/java/TempExample.java
+++ src/main/java/TempExample.java
@@ -3,51 +3,51 @@ import java.util.Scanner;
 
 public class TempExample {
 
-    // purpose: determine min and max values of temperatures read from stdin
+  // purpose: determine min and max values of temperatures read from stdin
 
-    public static void main(final String[] args) {
+  public static void main(final String[] args) {
 
-        var prompt = true;
-        if (args.length > 0 && "noprompt".equalsIgnoreCase(args[0])) {
-            prompt = false;
-        }
-        System.err.println("prompting: " + prompt);
+    var prompt = true;
+    if (args.length > 0 && "noprompt".equalsIgnoreCase(args[0])) {
+      prompt = false;
+    }
+    System.err.println("prompting: " + prompt);
 
-        final Iterator<String> input = new Scanner(System.in);
+    final Iterator<String> input = new Scanner(System.in);
 
-        // invoke the actual functionality
-        final var result = computeMinMax(input, prompt);
+    // invoke the actual functionality
+    final var result = computeMinMax(input, prompt);
 
-        // print result
-        System.out.println("max = " + result.getMax());
-        System.out.println("min = " + result.getMin());
+    // print result
+    System.out.println("max = " + result.getMax());
+    System.out.println("min = " + result.getMin());
+  }
+
+  public static MinMaxResult computeMinMax(final Iterator<String> input, final boolean prompt) {
+    var maxTemp = -Double.MAX_VALUE;
+    var minTemp = +Double.MAX_VALUE;
+
+    if (prompt) {
+      System.out.print("Enter a number: ");
+    }
+    while (input.hasNext()) {
+      // pull next string item from input
+      final String item = input.next();
+
+      // convert
+      final double temp = Double.parseDouble(item);
+
+      if (temp > maxTemp) {
+        maxTemp = temp;
+      }
+      if (temp < minTemp) {
+        minTemp = temp;
+      }
+      if (prompt) {
+        System.out.print("Enter a number: ");
+      }
     }
 
-    public static MinMaxResult computeMinMax(final Iterator<String> input, final boolean prompt) {
-        var maxTemp = -Double.MAX_VALUE;
-        var minTemp = +Double.MAX_VALUE;
-
-        if (prompt) {
-            System.out.print("Enter a number: ");
-        }
-        while (input.hasNext()) {
-            // pull next string item from input
-            final String item = input.next();
-
-            // convert
-            final double temp = Double.parseDouble(item);
-
-            if (temp > maxTemp) {
-                maxTemp = temp;
-            }
-            if (temp < minTemp) {
-                minTemp = temp;
-            }
-            if (prompt) {
-                System.out.print("Enter a number: ");
-            }
-        }
-
-        return new MinMaxResult(minTemp, maxTemp);
-    }
+    return new MinMaxResult(minTemp, maxTemp);
+  }
 }
